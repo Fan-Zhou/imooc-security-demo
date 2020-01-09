ce88642e4acbe3f14020a76395570a4e745dffbd
zhoufan <503541585@qq.com>
2020-01-09 17:56:11 +0800

    add aspect

---
+++ imooc-security-demo-release/src/main/java/cn/zhou/web/aspect/TimeAspect.java
@@ -0,0 +1,34 @@
+package cn.zhou.web.aspect;
+
+import cn.zhou.dto.User;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+
+
+@Aspect
+@Component
+public class TimeAspect {
+
+
+    @Around("execution(* cn.zhou.controller.*(..))")
+    public Object handleController(ProceedingJoinPoint pjp) throws Throwable {
+        System.out.println("before aspect ");
+
+        Object[] args = pjp.getArgs();
+        for(int i=0;i<args.length;i++){
+            System.out.println(args[i]);
+        }
+        Object object = pjp.proceed();
+//        object 是执行完controller 后 返回的值
+        if(object instanceof User){
+            User user = (User)object;
+            System.out.println(user);
+        }
+        System.out.println("after aspect");
+        return null;
+    }
+
+}
